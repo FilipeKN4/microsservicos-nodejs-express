8293773e7c7e7a76f3f2998a188329b8c00603bb
FilipeKN4 <filipekn4@gmail.com>
2021-03-12 00:08:39 -0300

    Utilização de requisição POST para envio de dados do formulário

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Microsserviços utilizando Node.js e Express.js
-Aplicação com o objetivo de reproduzir microsserviços utilizando Node.js e Express.js. Os microsserviços criados são:
+# Requisições utilizando Node.js e Express.js
+Aplicação com o objetivo de reproduzir requisições GET/POST utilizando Node.js e Express.js. Os serviços criados são:
 - Checkout
 - Pagamento
 - Cupom
+
+A aplicação utiliza Docker e Docker-Compose para a geração de um ambiente de Node.js com nodemon. 
--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
-
+// const urlencodedParser = bodyParser.urlencoded({ extended: false })
 // Arquivos estáticos
 app.use(express.static('public'))
-app.use("/css", express.static(__dirname + 'public/css'))
-app.use("/js", express.static(__dirname + 'public/js'))
+// app.use("/css", express.static(__dirname + 'public/css'))
+// app.use("/js", express.static(__dirname + 'public/js'))
+app.use(express.json());
+app.use(express.urlencoded());
 
 // Template Engine
 // app.use(expressLayouts)
@@ -18,6 +20,7 @@ app.use("/js", express.static(__dirname + 'public/js'))
 app.set('view engine', 'ejs')
 
 // Navegação
+// GET Requests
 app.get('/', (req, res) => {
     res.redirect('/checkout')
 })
@@ -39,6 +42,14 @@ app.get("*", (req, res) => {
     // definir erro para todas as rotas não existentes
     res.send("PAGE NOT FOUND"); 
   });
+
+// POST requests
+app.post('/process', (req, res) => {
+    console.log(req.body)
+    
+    res.render('pages/checkout')
+    
+})
 
 // escutar na porta 3000
 app.listen(PORT, HOST);
